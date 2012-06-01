010a924f9045caf23c277448a2b012327edb4f8c
James Almond <james@jamesalmond.com>
2012-06-01 06:35:57 +0100

    Added document ready to js

--- app/assets/javascripts/refinerycms-news.js
+++ app/assets/javascripts/refinerycms-news.js
@@ -1,3 +1,5 @@
-$('#news_filter select').change(function(){
-  $('#news_filter').submit();
+$(document).ready(function(){
+  $('#news_filter select').change(function(){
+    $('#news_filter').submit();
+  });
 });
